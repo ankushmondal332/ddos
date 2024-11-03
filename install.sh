7919dd42aa625147275cecc50d4e2f49d431d413
ankushmondal332 <mondalankush2336@outlook.com>
2024-11-03 16:25:05 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/privaye-dd/Danger.py",
+    "postStartCommand": "python3 /workspaces/ddos/bot.py",
     "customizations": {
         "vscode": {
             "settings": {
